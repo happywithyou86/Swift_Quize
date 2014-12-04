5026d87f80a67394b8e8e5e02140692ee214076c
Bharat Jadav <bharat@tsg-ahmds-Mac-mini.local>
2014-12-04 13:58:32 +0530

    Initial Commit

---
+++ TabBar-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabBar-Swift
+//
+//  Created by Bharat Jadav on 04/12/14.
+//  Copyright (c) 2014 Bharat Jadav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
